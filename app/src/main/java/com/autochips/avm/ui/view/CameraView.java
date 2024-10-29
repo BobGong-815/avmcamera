a5644ac7966c00dd1f95de6f49e60b959afb450d
lyl <chenwei@luckybarm.com>
2024-10-29 21:04:10 +0800

    修改后视镜切换档位选中问题

--- app/src/main/java/com/autochips/avm/ui/view/CameraView.java
+++ app/src/main/java/com/autochips/avm/ui/view/CameraView.java
@@ -1026,6 +1026,7 @@ public class CameraView extends View implements LifecycleOwner {
             }
             KLog.d("tabSelectListener settingView = " + isChangeGear);
             if (settingView.getVisibility() == View.VISIBLE) {
+                llSetting.setSelected(false);
                 settingView.setVisibility(GONE);
                 liftBg.setVisibility(GONE);
             }
@@ -1035,6 +1036,7 @@ public class CameraView extends View implements LifecycleOwner {
                 liftBg.setVisibility(GONE);
             }
             if (rearviewMirrorView.getVisibility() == VISIBLE) {
+                llBackMirror.setSelected(false);
                 rearviewMirrorView.setVisibility(View.GONE);
                 liftBg.setVisibility(GONE);
             }
