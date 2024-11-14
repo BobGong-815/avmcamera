276ea95e94dadc03ec4a039f1f09670b56b37cd6
龚孝波 <11532298+gongxiaobo_815@user.noreply.gitee.com>
2024-11-14 15:11:58 +0800

    Filter invalid turn lamp reset signal.

--- app/src/main/java/com/autochips/avm/service/AvmRuntime.java
+++ app/src/main/java/com/autochips/avm/service/AvmRuntime.java
@@ -1345,12 +1345,15 @@ public class AvmRuntime {
     public void turnLampChange(int direction) {
         if (direction == 0) {
             synchronized (syncObj) {
-                setOverExitFlag(0);
-                dataSts.events.add(DataDefine.EVT_TURN_LAMP_RESET);
-                dataSts.turnLampResetTime = System.currentTimeMillis();
-                dataSts.lastChangeTime = System.currentTimeMillis();
+                if (dataSts.sensors[0] == DataDefine.SENSOR_TURN_LAMP || dataSts.sensors[0] == DataDefine.SENSOR_RADAR_TURN_LAMP) {
+                    // 有时不在转向激活状态，也会收到转向复位信号
+                    setOverExitFlag(0);
+                    dataSts.events.add(DataDefine.EVT_TURN_LAMP_RESET);
+                    dataSts.turnLampResetTime = System.currentTimeMillis();
+                    dataSts.lastChangeTime = System.currentTimeMillis();
 
-                syncObj.notify();
+                    syncObj.notify();
+                }
             }
         } else if (direction == 1) {
             synchronized (syncObj) {
@@ -1991,6 +1994,14 @@ public class AvmRuntime {
                     }
                 }
 
+            }
+
+            if (dataSts.timing30sFlag) {
+                if (dataSts.events.contains(DataDefine.EVT_TURN_LAMP_ACTIVE)
+                    || dataSts.events.contains(DataDefine.EVT_RADAR_ACTIVE)) {
+                    KLog.d("Turn lamp active or radar active blocked 30s exit.");
+                    dataSts.sensorBlockPExit = true;
+                }
             }
             dataSts.timing30sFlag = false;
             KLog.d("+++ set time30sFlag to false.");
