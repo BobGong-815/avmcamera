2fe833b34ef20611f210a54d8dd4aac24e51c06c
龚孝波 <11532298+gongxiaobo_815@user.noreply.gitee.com>
2025-05-28 11:28:58 +0800

    倒挡2D记忆广角后视

--- app/src/main/java/com/autochips/avm/ui/view/CameraView.java
+++ app/src/main/java/com/autochips/avm/ui/view/CameraView.java
@@ -1472,6 +1472,7 @@ public class CameraView extends View implements LifecycleOwner {
     private void inputViewModel() {
 //      KLog.d(isDismissView+" isDismissView hisModel= " + hisModel +"hisPosition= "+hisPosition + "viewPosition="+ viewPosition);
         KLog.d("inputViewModel()");
+        /*
         if (isDismissView && hisPosition == 2) {
 
             if (AvmRuntime.self().isRearGearSts()) {
@@ -1495,6 +1496,7 @@ public class CameraView extends View implements LifecycleOwner {
                 return;
             }
         }
+        */
         viewShowStatus();
     }
 
